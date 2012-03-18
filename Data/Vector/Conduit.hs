856e0b503a3a92bd4770037ad773f95bb78d5632
Jared Hance <jaredhance@gmail.com>
2012-03-18 18:04:55 -0500

    Reimplement consumeVector and remove dependency on DList

--- Data/Vector/Conduit.hs
+++ Data/Vector/Conduit.hs
@@ -12,7 +12,6 @@ import Control.Monad.Primitive
 import Control.Monad.ST.Safe
 import Control.Monad.Trans.Class
 import Data.Conduit
-import qualified Data.DList as D
 import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Generic.Mutable as M
 
@@ -32,13 +31,23 @@ sourceMVector v = sourceState 0 f
                                    return $ StateOpen (index + 1) x
 
 -- | Consumes all values from the stream and return as an immutable vector.
--- Works by creating a DList.
-consumeVector :: (Resource m, V.Vector v a) => Sink a m (v a)
-consumeVector = sinkState D.empty push close
-    where push xs x = return . StateProcessing $ D.snoc xs x
-          close xs = return . V.fromList . D.toList $ xs
+consumeVector :: (PrimMonad m, Resource m, V.Vector v a)
+                 => Sink a m (v a)
+consumeVector = sinkState (Nothing, 0) push close
+    where push (v, index) x = do v' <- case v of
+                                        Nothing -> lift $ M.new 10
+                                        Just vec -> return vec
+                                 let len = M.length v'
+                                 v'' <- if index >= len
+                                            then lift $ M.grow v' len
+                                            else return v'
+                                 lift $ M.write v'' index x
+                                 return $ StateProcessing (Just v'', index + 1)
+          close (Nothing, index) = return $ V.fromList []
+          close (Just v, index) = do let v' = M.take index v
+                                     lift $ V.unsafeFreeze v'
 
--- | Consume all values from the stream and return as a mutable vector.
+-- | Consumes all values from the stream and returns as a mutable vector.
 consumeMVector :: (PrimMonad m, Resource m, M.MVector v a)
                   => Sink a m (v (PrimState m) a)
 consumeMVector = sinkState (Nothing, 0) push close
