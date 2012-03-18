8c010ea79a5e102e42971f1f1e9fd790c1292363
Jared Hance <jaredhance@gmail.com>
2012-03-18 17:23:08 -0500

    Tests

---
+++ test/Main.hs
@@ -0,0 +1,30 @@
+module Main (main)
+where
+
+import Control.Monad.ST
+import Data.Conduit
+import qualified Data.Vector as V
+import Data.Vector.Conduit
+import Test.QuickCheck
+import qualified Test.Framework as F
+import Test.Framework.Runners.Console (defaultMain)
+import Test.Framework.Providers.QuickCheck2
+
+testInverse :: [Int] -> Bool
+testInverse l = runST $ do let v = V.fromList l
+                           v' <- runResourceT $ sourceVector v $$ consumeVector
+                           return $ v == v'
+
+testInverse2 :: [Int] -> Bool
+testInverse2 l = runST $ do let v = V.fromList l
+                            sink <- consumeMVector
+                            v' <- runResourceT $ sourceVector v $$ sink
+                            v'' <- V.unsafeFreeze v'
+                            return $ v == v''
+
+tests :: [F.Test]
+tests = [testProperty "Inverse" testInverse,
+         testProperty "Invers2" testInverse2]
+
+main :: IO ()
+main = defaultMain tests
