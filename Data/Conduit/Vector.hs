08ae9d4e2a4d340531c00c2ba2ccd4c5cd2ef574
Jared Hance <jaredhance@gmail.com>
2012-03-21 18:56:44 -0500

    Fixes for conduit-0.3

--- Data/Conduit/Vector.hs
+++ Data/Conduit/Vector.hs
@@ -20,91 +20,94 @@ import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Generic.Mutable as M
 
 -- | Use an immutable vector as a source.
-sourceVector :: (Resource m, V.Vector v a) => v a -> Source m a
+sourceVector :: (Monad m, V.Vector v a) => v a -> Source m a
 sourceVector v = sourceState 0 f
     where f index | index == V.length v = return StateClosed
                   | otherwise = return $ StateOpen (index + 1) (v V.! index)
 
 -- | Use a mutable vector as a source in the ST or IO monad.
-sourceMVector :: (PrimMonad m, Resource m, M.MVector v a)
+sourceMVector :: (PrimMonad m, M.MVector v a)
                  => v (PrimState m) a
                  -> Source m a
 sourceMVector v = sourceState 0 f
     where f index | index == M.length v = return StateClosed
-                  | otherwise = do x <- lift $ M.read v index
+                  | otherwise = do x <- M.read v index
                                    return $ StateOpen (index + 1) x
 
 -- | Consumes all values from the stream and return as an immutable vector.
-consumeVector :: (PrimMonad m, Resource m, V.Vector v a)
+-- Due to the way it operates, it requires the ST monad at the minimum,
+-- although it can also operate IO. This is due to its dependency on
+-- a mutable vector.
+consumeVector :: (PrimMonad m, V.Vector v a)
                  => Sink a m (v a)
 consumeVector = sinkState (Nothing, 0) push close
     where push (v, index) x = do v' <- case v of
-                                        Nothing -> lift $ M.new 10
+                                        Nothing -> M.new 10
                                         Just vec -> return vec
                                  let len = M.length v'
                                  v'' <- if index >= len
-                                            then lift $ M.grow v' len
+                                            then M.grow v' len
                                             else return v'
-                                 lift $ M.write v'' index x
+                                 M.write v'' index x
                                  return $ StateProcessing (Just v'', index + 1)
           close (Nothing, index) = return $ V.fromList []
-          close (Just v, index) = lift . V.unsafeFreeze $ M.take index v
+          close (Just v, index) = V.unsafeFreeze $ M.take index v
 
 -- | Consumes the first n values from a source and returns as an immutable
 -- vector.
-takeVector :: (PrimMonad m, Resource m, V.Vector v a)
+takeVector :: (PrimMonad m, V.Vector v a)
               => Int -> Sink a m (v a)
 takeVector n = sinkState (Nothing, 0) push close
     where push (v, index) x = do
             v' <- case v of
-                    Nothing -> lift $ M.new n
+                    Nothing -> M.new n
                     Just vec -> return vec
             if index >= n
-                then do v'' <- lift $ V.unsafeFreeze v'
+                then do v'' <- V.unsafeFreeze v'
                         return $ StateDone Nothing v''
-                else do lift $ M.write v' index x
+                else do M.write v' index x
                         return $ StateProcessing (Just v', index + 1)
           close (Nothing, index) = return $ V.fromList []
-          close (Just v, index) = lift $ V.unsafeFreeze v
+          close (Just v, index) = V.unsafeFreeze v
 
 -- | Consumes all values from the stream and returns as a mutable vector.
-consumeMVector :: (PrimMonad m, Resource m, M.MVector v a)
+consumeMVector :: (PrimMonad m, M.MVector v a)
                   => Sink a m (v (PrimState m) a)
 consumeMVector = sinkState (Nothing, 0) push close
     where push (v, index) x = do v' <- case v of
-                                        Nothing -> lift $ M.new 10
+                                        Nothing -> M.new 10
                                         Just vec -> return vec
                                  let len = M.length v'
                                  v'' <- if index >= len
-                                            then lift $ M.grow v' len
+                                            then M.grow v' len
                                             else return v'
-                                 lift $ M.write v'' index x
+                                 M.write v'' index x
                                  return $ StateProcessing (Just v'', index + 1)
-          close (Nothing, index) = lift $ M.new 0
+          close (Nothing, index) = M.new 0
           close (Just v, index) = return $ M.take index v
 
 -- | Consumes the first n values from the stream and returns as a
 -- mutable vector.
-takeMVector :: (PrimMonad m, Resource m, M.MVector v a)
+takeMVector :: (PrimMonad m, M.MVector v a)
                => Int -> Sink a m (v (PrimState m) a)
 takeMVector n = sinkState (Nothing, 0) push close
     where push (v, index) x =
             do v' <- case v of
-                        Nothing -> lift $ M.new n
+                        Nothing -> M.new n
                         Just vec -> return vec
                if index >= n
                     then return $ StateDone Nothing v'
-                    else do lift $ M.write v' index x
+                    else do M.write v' index x
                             return $ StateProcessing (Just v', index + 1)
-          close (Nothing, index) = lift $ M.new 0
+          close (Nothing, index) = M.new 0
           close (Just v, index) = return v
 
 -- | Conduit which thaws immutable vectors into mutable vectors
-thawConduit :: (PrimMonad m, Resource m, V.Vector v a)
+thawConduit :: (PrimMonad m, V.Vector v a)
                 => Conduit (v a) m (V.Mutable v (PrimState m) a)
 thawConduit = L.mapM V.unsafeThaw
 
 -- | Conduit which freezes mutable vectors into immutable vectors
-freezeConduit :: (PrimMonad m, Resource m, V.Vector v a)
+freezeConduit :: (PrimMonad m, V.Vector v a)
                  => Conduit (V.Mutable v (PrimState m) a) m (v a)
 freezeConduit = L.mapM V.unsafeFreeze
--- test/Main.hs
+++ test/Main.hs
@@ -3,8 +3,8 @@ where
 
 import Control.Monad.ST
 import Data.Conduit
+import Data.Conduit.Vector
 import qualified Data.Vector as V
-import Data.Vector.Conduit
 import Test.QuickCheck
 import qualified Test.Framework as F
 import Test.Framework.Runners.Console (defaultMain)
@@ -12,19 +12,19 @@ import Test.Framework.Providers.QuickCheck2
 
 testInverse :: [Int] -> Bool
 testInverse l = runST $ do let v = V.fromList l
-                           v' <- runResourceT $ sourceVector v $$ consumeVector
+                           v' <- sourceVector v $$ consumeVector
                            return $ v == v'
 
 testInverse2 :: [Int] -> Bool
 testInverse2 l = runST $ do let v = V.fromList l
-                            v' <- runResourceT $ sourceVector v $$ consumeMVector
+                            v' <- sourceVector v $$ consumeMVector
                             v'' <- V.unsafeFreeze v'
                             return $ v == v''
 
 testInverse3 :: [[Int]]-> Bool
 testInverse3 ls = runST $ do let vs = map V.fromList ls
                              let v = V.fromList vs
-                             v' <- runResourceT $ sourceVector v $= thawConduit $= freezeConduit $$ consumeVector
+                             v' <- sourceVector v $= thawConduit $= freezeConduit $$ consumeVector
                              return $ v == v'
 
 tests :: [F.Test]
